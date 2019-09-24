c29c821cf9b555004b5298c2906719088beb8ffd
Ethan <ethanw123@gmail.com>
2019-09-24 13:58:12 -0400

    Fix parts of TrafficLight so light switching can work

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -11,11 +11,12 @@ T MessageQueue<T>::receive()
     // to wait for and receive new messages and pull them from the queue using move semantics. 
     // The received object should then be returned by the receive function. 
     std::unique_lock<std::mutex> uLock(_mtx);
-    //TrafficLight::_condition.wait();
+    TrafficLight::_condition.wait(uLock);
 
     //Move semantics here
-    
+    std::move(TrafficLight::_currentPhase);
     //return recieved object here
+
 }   
 
 template <typename T>
@@ -25,7 +26,7 @@ void MessageQueue<T>::send(T &&msg)
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
     std::lock_guard<std::mutex> uLock(_mtx);
     
-    //_queue.emplace_back(std::move(msg));
+    _queue.emplace_back(std::move(msg));
     
     _var.notify_one();
 }
@@ -45,6 +46,7 @@ void TrafficLight::WaitForGreen()
     // Once it receives TrafficLightPhase::green, the method returns.
     while(true){
         MessageQueue<TrafficLightPhase> receive();
+        break;
     }
 }
 
@@ -57,13 +59,14 @@ void TrafficLight::Simulate()
 {
     // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
     
-    std::thread(CycleThroughPhases());
+    threads.emplace_back(std::thread(&TrafficLight::CycleThroughPhases, this));
 }
 
 
 // virtual function which is executed in a thread
 void TrafficLight::CycleThroughPhases()
 {
+    std::cout<<"Cycle called!"<<std::endl;
     // FP.2a : Implement the function with an infinite loop that measures the time between two loop cycles 
     // and toggles the current phase of the traffic light between red and green and sends an update method 
     // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
@@ -80,21 +83,16 @@ void TrafficLight::CycleThroughPhases()
         long timeSinceLastUpdate = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - lastUpdate).count();
         
         //Sleep for 4 to 6 seconds using rand(), Right way to implement it?
-        std::this_thread::sleep_for(std::chrono::seconds(rand() % 2 + 4));
-
+        std::this_thread::sleep_for(std::chrono::seconds(rand() % 4 + 2));
+        
         //Toggle the current phase of the traffic light, if else statements the way to go?
         if(_currentPhase == TrafficLightPhase::red)
             _currentPhase = TrafficLightPhase::green;
         
         else if(_currentPhase == TrafficLightPhase::green)
             _currentPhase = TrafficLightPhase::red;
-
+    
         //send an update method to the message queue using move semantics, new light, and timeSinceLastUpdate
-        //Look at the monitor object pattern, 2 of 6
-            //Message queue in Intersection::addVehicleToQueue
-        //std::lock_guard<std::mutex> uLock(_mutex);
-        //MessageQueue<TrafficLightPhase> lightMessage;
-        //MessageQueue<TrafficLightPhase>::_queue.send(std::move(_currentPhase));
 
         //Push each new TrafficLightPhase into _message by calling send into conjuction with move semantics
         _message.send(std::move(_currentPhase)); 
